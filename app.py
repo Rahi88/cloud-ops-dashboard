4937aa79dc425a570a9fe91527d29f9626ea2f1d
Rahimunnisa <shaik.rahimunnisa@capgemini.com>
2026-05-22 11:24:10 +0530

    updated

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ app = Flask(__name__)
 CORS(app)  # Unlocks the security gate for your web browser interface
 
 # 1. PASTE YOUR ACTUAL UBUNTU INSTANCE ID HERE
-INSTANCE_ID = "i-0bb9665c0696668cd" 
+INSTANCE_ID = "i-00xxxxxxxxxxx" 
 
 # 2. Setting up the Boto3 EC2 client (us-east-1 for N. Virginia)
 ec2 = boto3.client('ec2', region_name='us-east-1')
 
+#method to start server
 @app.route('/start-server', methods=['POST'])
 def start_server():
     try:
@@ -20,12 +21,23 @@ def start_server():
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 400
 
+#method to stop server
 @app.route('/stop-server', methods=['POST'])
 def stop_server():
     try:
         # Boto3 core command to turn OFF an EC2 instance
         ec2.stop_instances(InstanceIds=[INSTANCE_ID])
         return jsonify({"status": "success", "message": f"Server {INSTANCE_ID} stop command issued!"})
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 400
+        
+#method to terminate server
+@app.route('/terminate-server', methods=['POST'])
+def terminate_server():
+    try:
+        # Boto3 core command to turn OFF an EC2 instance
+        ec2.terminate_instances(InstanceIds=[INSTANCE_ID])
+        return jsonify({"status": "success", "message": f"Server {INSTANCE_ID} terminate command issued!"})
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 400
 
